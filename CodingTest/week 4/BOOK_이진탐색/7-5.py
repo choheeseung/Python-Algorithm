b9be71fba66917ba80d79dbecd745900c5aba27c
choheeseung <gmltmd0816@naver.com>
2022-01-09 22:12:58 +0900

    22.01.09

---
+++ CodingTest/week 4/BOOK_이진탐색/7-5.py
@@ -0,0 +1,24 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+n = int(input())
+n_list = list(map(int, input().split()))
+
+m = int(input())
+m_list = list(map(int, input().split()))
+
+for i in m_list:
+    result = binary_search(n_list, i, 0, n - 1)
+    if result != None:
+        print("yes", end=" ")
+    else:
+        print("no", end=" ")
